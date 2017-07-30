11c7efbc05f75a8d553ebb88d51857c49da779a9
Animania260 <animania260@gmail.com>
2017-07-29 23:30:50 -0400

    Samsung source push: VPU1AQB9

---
+++ build_kernel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+OUT_DIR=out
+
+# you should change the "CROSS_COMPILE" to right toolchain path (you downloaded)
+# ex)CROSS_COMPILE={android platform directory you downloaded}/android/prebuilt/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+
+COMMON_ARGS="-C $(pwd) O=$(pwd)/${OUT_DIR} ARCH=arm64 CROSS_COMPILE=aarch64-linux-android-"
+
+export PATH=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PATH 
+
+[ -d ${OUT_DIR} ] && rm -rf ${OUT_DIR}
+mkdir ${OUT_DIR}
+
+make ${COMMON_ARGS} msm8953_sec_defconfig VARIANT_DEFCONFIG=msm8953_sec_j7poplte_usa_spr_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make ${COMMON_ARGS}
+
+cp ${OUT_DIR}/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+
